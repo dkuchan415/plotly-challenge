cbf6d5065cf6cdf9966e1fb179e75ee0bec58443
dkuchan415 <danielkuchan@yahoo.com>
2020-12-29 19:27:46 -0600

    Completed metadata table

--- static/js/app.js
+++ static/js/app.js
@@ -2,36 +2,23 @@
 
 const url = "data/samples.json"
 
-
 // init function
 function init() {
+    var dropdown = d3.select("#selDataset");
     d3.json(url).then(function(data) {
         var namesArray = data.names;
-        var dropdown = d3.select("#selDataset");
         namesArray.forEach(function(name) {
             var option = dropdown.append("option");
             option.property("value", name);
             option.text(name);
         });
+        //Call both update functions
+
         updatePlotly(namesArray[0]);
+        updateDemo(namesArray[0]);
     });
 }
 
-init()
-
-//Update all of the plots any time that a new sample is selected.
-
-d3.selectAll("#selDataset").on("change", updatePage)
-
-function updatePage() {
-    var dropdown = d3.select("#selDataset");
-      // Assign the dropdown menu item ID to a variable
-    var selectedOption = dropdown.property("value");
-    // Assign the dropdown menu option to a variable
-
-    console.log(selectedOption);
-    updatePlotly();
-};
 
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs 
 //found in that individual. Act. 15-03-06
@@ -41,25 +28,16 @@ function updatePlotly(name) {
         var filteredSampleData = data.samples.filter((sample) => sample.id === name)[0];
         console.log(filteredSampleData);
         // console.log("updatePlotly -> name type", typeof(name) )
-        var filteredMetaData = data.metadata.filter((meta) => String(meta.id) === name)[0];
-        console.log(filteredMetaData);
         var sample_values = filteredSampleData.sample_values;
         var otu_ids = filteredSampleData.otu_ids;
         var otu_labels = filteredSampleData.otu_labels;
-        var id = filteredMetaData.id;
-        var ethnicity = filteredMetaData.ethnicity;
-        var gender = filteredMetaData.gender;
-        var age = filteredMetaData.age;
-        var location = filteredMetaData.location;
-        var bbtype = filteredMetaData.bbtype;
-        var wfreq = filteredMetaData.wfreq;
 
         //Bar Chart
 
-        var sliced_values = filteredSampleData.sample_values.slice(0, 10).reverse()
+        var sliced_values = sample_values.slice(0, 10).reverse();
         //make a string
-        var sliced_otu_ids = String(filteredSampleData.otu_ids.slice(0, 10).reverse())
-        var sliced_otu_labels = filteredSampleData.otu_labels.slice(0, 10).reverse()
+        var sliced_otu_ids = otu_ids.slice(0, 10).map(otuid => `OTU ${otuid}`).reverse();
+        var sliced_otu_labels = otu_labels.slice(0, 10).reverse();
 
         var trace1 = {
             x: sliced_values,
@@ -103,31 +81,51 @@ function updatePlotly(name) {
 
     //Display the sample metadata, i.e., an individual's demographic information.
 
-        d3.select("panel-body")
-            .select("#table")
-            .data(filteredMetaData)
-            .enter()
-            .append("tr")
-            .html(function(md) {
-                return `<td>${md.id}</td><td>${md.ethnicity}</td><td>${md.gender}</td><td>${md.age}</td><td>${md.location}</td><td>${md.bbtype}</td><td>${md.wfreq}</td>`
-            });
-
-        // filteredMetaData.forEach(function(metaTable) {
-        //     console.log(metaTable);
-        // })
-
 });
-
 }
 
 
+//Create a function to update the demographic info table.
+///Display the sample metadata, i.e., an individual's demographic information.
+
+
+function updateDemo(name) {
+    d3.json(url).then(function(data) {
+        console.log(data);
+        var metadata = data.metadata;
+        console.log(metadata);
+        var filteredMetaData = data.metadata.filter((meta) => meta.id.toString() === name)[0];
+        console.log(filteredMetaData);
+        var tbody = d3.select("#sample-metadata");
+        tbody.html("");
+        Object.entries(filteredMetaData).forEach(function([key, value]) {
+            console.log(key, value);
+            tbody.append("tr").text(`${key}: ${value}`);
+        });
+    });
+}
+
 //Update all of the plots any time that a new sample is selected.
 
-// d3.selectAll("#selDataset").on("change", updatePlotly);
+d3.selectAll("#selDataset").on("change", updatePage);
 
-// function updatePlotly() {
-//     var dropdown = d3.select("#selDataset")
-//         .append;
-//     var dataset = dropdown.property("value");
-// };
+function updatePage() {
+    // var dropdown = d3.select("#selDataset");
+
+    // //Clear existing data
+
+    // dropdown.html("");
+    //   // Assign the dropdown menu item ID to a variable
+    // var selectedOption = dropdown.property("value");
+    // // Assign the dropdown menu option to a variable
+
+    // console.log(selectedOption);
+    updatePlotly(name);
+    updateDemo(name);
+};
+
+// Run init function
+
+init();
+
 
